d896bbe313c9eff306bf41e73dbebd733bef8578
MGelmane <marta.gelmane@gmail.com>
2022-10-03 21:55:15 +0100

    ..

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+
+window.alert("Welcome!"); //Pop up window with message 'Welcome' upon entering the page
+
+var Name = "Maya"; //Defining a varibale and assigning it a string value
+var color = "red"; //Defining a varibale and assigning it a string value
+var color = color.fontcolor("red"); //Defining font color for the variable 'color'
+
+document.write(Name + '\'s favourite color is ' + color + '.'); // Concatenating both variables with an extra text in the middle
+
+var assignment = "8"; //Defining a variable
+var points = "12"; //Defining a variable
+document.write(' She has completed ' + assignment + ' assignments and scored a total of ' + assignment * points + ' points'); //Concatenating varibales and adding an expression which returns a value of total points on all assignments
+
+
+
+
+
+
+
+
